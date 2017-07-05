87591a6484e3c47cbbffea04c7111b4fb357c279
doulighan <doulighan@gmail.com>
2017-07-05 13:39:13 -0400

    fsdc

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class CocktailsController < ApplicationController
+      before_action :set_cocktail, only: [:show, :update, :destroy]
+
       def index
         render json: Cocktail.all
       end
@@ -10,7 +12,12 @@ module Api
       end
 
       def create
-
+        @cocktail = Cocktail.new(cocktail_params)
+        if @cocktail.save 
+          render json: @cocktail
+        else
+          render json: @cocktail.errors, status: :unprocessable_entity
+        end
       end
 
       def edit
@@ -23,6 +30,16 @@ module Api
 
       def destroy
 
+      end
+
+    private 
+
+      def set_cocktail
+        @cocktail = Cocktail.find(params[:id])
+      end
+
+      def cocktail_params
+        params.require(:cocktail).permit(:name, :description)
       end
     end
   end
